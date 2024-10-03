38bd4aed766d8b63830994181ce32e976d33fcdd
Divith123 <divithselvam23@gmail.com>
2024-10-03 02:40:01 -0700

    Style Changed

--- obj/Debug/net8.0/OpenWeather.AssemblyInfo.cs
+++ obj/Debug/net8.0/OpenWeather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenWeather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9314835e6f227e3ba546e57f2de1f60f5446a901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a7864bdd7a9fa2536ab98e3fdc712619ecce4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenWeather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenWeather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
